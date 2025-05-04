7308839f7ef7771f20ad063e2cd8b39c4d1fdff1
Gul <gngulnayab@gmail.com>
2025-05-03 21:34:39 -0700

    refactor: change code and remove extra Lines

--- frontend/app/Home.js
+++ frontend/app/Home.js
@@ -41,7 +41,6 @@ export default function Home() {
                 </Card>
               );
             })}
-            ;
           </Stack>
         </Card>
       </Link>
--- frontend/lib/mongo.js
+++ frontend/lib/mongo.js
@@ -1,19 +1,19 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
+
 const options = {};
 
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+if (!uri) {
+  throw new Error('Please add MONGODB_URI to .env.local');
 }
 
 if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options);
   global._mongoClientPromise = client.connect();
-  console.log('You successfully connected to MongoDB');
 }
 clientPromise = global._mongoClientPromise;
 
